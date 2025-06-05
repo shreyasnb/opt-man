bb0c5e022b96323d22040fb93471bc8b42c54a6b
Shreyas NB <shreyasbharadwaj04@gmail.com>
2025-06-05 22:44:15 +0530

    reformat code for any [n,k]

--- Robust-Least-Squares/LSQGradDescent.m
+++ Robust-Least-Squares/LSQGradDescent.m
@@ -3,78 +3,99 @@ path('../utils',oldpath)
 
 setup();
 % rng(42, 'twister');
-n = 2;
-k = 1;
-
-tolx = 1e-12;
-N = 1;
-alpha_0 = 1e-2;
-r = 1e-4;
-rho = sin(2*pi/8);
-theta = asin(rho);
-theta_0 = 0;
-theta_x = 10*pi/180;
-theta_b = 2*pi/16;
-lambda = 0.1;
-tau = 0.1;
 
 gradf_store = [];
+x_store = [];
+proj_x_store = [];
+f_store = [];
 alpha_store = [];
-alpha = alpha_0;
 
-problem.dim1 = n;
-problem.dim2 = k;
-problem.data.theta_0 = theta_0;
-problem.data.theta_b = theta_b;
-problem.params.rho = rho;
-problem.params.theta = theta;
+%% Problem initialization
+problem.dim = [2,2];
+problem.data.theta_0 = 0*pi/180;
+problem.data.theta_b = 30*pi/180;
+problem.params.alpha = 1e-2;
+problem.params.tolx = 1e-12;
+problem.params.rho = sin(45*pi/180);
+problem.params.lambda = 0.2; 
 
-[b, x_old, Y, Y_hat] = init(problem);
+problem = init(problem);
 
-problem.cost = @(x) f(x,Y,Y_hat, b, lambda);
-problem.grad = @(x) gradf(x,Y,b);
-problem.data.b = b;
-problem.data.Y_hat = Y_hat;
+problem.cost = @(x,y) f(x,problem.Y,problem.data.b);
+problem.grad = @(x,y) gradf(x,problem.Y, problem.data.b);
+problem.Bx = @(x) B(x, problem);
 
 % checkgradient(problem);
 
-v = gradf(x_old, Y, b);
-x_new = x_old - alpha*v;
+%% Gradient Descent
+results = GDA(problem);
 
-while (N<=1e5)
-    v = gradf(x_old, Y, b);
-    gradf_store = [gradf_store, v];
-    % alpha_store = [alpha_store, alpha];
-    % alpha = alpha_0;
-    % while true
-    %     if((f(x_old,Y, Y_hat, b, lambda)-f((x_old-alpha*v), Y, Y_hat, b, lambda))< -r*alpha*norm(v)^2)
-    %         break
-    %     else
-    %         alpha = tau*alpha;
-    %     end
-    % end
-    if(norm(v)< tolx)
-        break
-    else
-        x_old = x_new;
-        x_new = x_old - alpha*v;
-        Bx = B(x_new, b, Y_hat, lambda);
-        [Y,~] = eigs(Bx, k);
-    end
-    N = N+1;
-end
+%% Plot results
+plot_gradf(results);
+plot_cost(results, problem);
+plot_optimal_sol(results, problem);
 
-plot_gradf(gradf_store);
-plot_optimal_sol(x_new, Y, problem);
 
-function fun = f(x, Y, Y_hat, b, lambda)
-    fun = trace(Y'*B(x,b,Y_hat, lambda)*Y);
+%% Functions
+function fun = f(x, Y,b)
+    fun = norm(Y*(Y'*x) - b)^2;
 end
 
 function g = gradf(x, Y, b)
-    g = 2*(Y*Y')*(x-b);
+    g = 2*Y*(Y'*(x-b));
 end
 
-function Bout = B(x, b, Y_hat, lambda)
-    Bout = x*x' - b*x' - x*b' + lambda*(Y_hat*Y_hat');
+function Bx = B(x, problem)
+    b = problem.data.b;
+    Y_hat = problem.data.Y_hat;
+    lambda = problem.params.lambda;
+    Bx= x*x' - b*x' - x*b' + lambda*(Y_hat*Y_hat');
+end
+
+function out = GDA(problem)
+    N = 1;
+    alpha = problem.params.alpha;
+    tolx = problem.params.tolx;
+    k = problem.dim(2);
+    Y_hat = problem.data.Y_hat;
+    b = problem.data.b;
+    lambda = problem.params.lambda;
+    x_old = problem.x0;
+
+    out.gradf_store = [];
+    out.f_store = [];
+    out.x_store = [];
+    out.proj_x_store = [];
+
+    while (N<=1e5)
+        B = problem.Bx(x_old);
+        [Y,~] = eigs(B, k);
+        v = gradf(x_old, Y,b);
+    
+        out.gradf_store = [out.gradf_store, v];
+        out.f_store = [out.f_store, f(x_old, Y,b)];
+        out.x_store = [out.x_store, x_old];
+        out.proj_x_store = [out.proj_x_store, Y*(Y'*x_old)];
+        
+        if(norm(v)< tolx)
+            break
+        else
+            x_new = x_old - alpha*v;
+            x_old = x_new;
+        end
+        N = N+1;
+    end
+    out.Y_star = Y;
+    out.x_star = x_old;
+
+    if(N<=1e5)
+        disp('Gradient descent converged successfully')
+        disp(strcat('Number of iterations:  ',int2str(N)));
+        disp(strcat('Norm of gradient:  ', num2str(norm(v))));
+    else
+        if(norm(v)>tolx)
+            disp('Number of iterations exceeded! \n')
+            disp('Gradient descent failed within 1e5 iterations');
+        end
+    end
 end
